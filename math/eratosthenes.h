9114f47158a28ae8c366c5423512462fa2e338b0
Takaaki Hiragushi <hiragushi@hpcs.cs.tsukuba.ac.jp>
2014-03-22 01:35:10 +0900

    Fix interface of math/eratosthenes

--- math/eratosthenes.h
+++ math/eratosthenes.h
@@ -18,22 +18,39 @@ namespace math {
 /**
  *  @brief エラトステネスの篩による素数列挙
  *
- *  [0, RANGE) の範囲に含まれる素数集合を可変長配列・ビット集合の形で返す。
+ *  [0, n) の範囲に含まれる素数集合を可変長配列・ビット集合の形で返す。
+ *  計算量は \f$ \mathcal{O}(n \log{\log{n}}) \f$程度。
  *
- *  @tparam        RANGE  列挙される値の範囲
+ *  @tparam        T      篩のデータ型
  *  @param[in,out] sieve  素数集合のビット集合による表現
  *  @return        素数集合の可変長配列による表現
  */
-template <int RANGE>
-vector<int> eratosthenes(bitset<RANGE> &sieve = bitset<RANGE>()){
-	sieve[0] = sieve[1] = true;
+template <typename T>
+vector<int> eratosthenes(T &sieve){
+	const int n = sieve.size();
+	for(int i = 0; i < n; ++i){ sieve[i] = 1; }
+	sieve[0] = sieve[1] = 0;
 	vector<int> primes;
-	for(int i = 2; i < RANGE; ++i){
-		if(sieve[i]){ continue; }
+	for(int i = 2; i < n; ++i){
+		if(!sieve[i]){ continue; }
 		primes.push_back(i);
-		for(int j = i + i; j < RANGE; j += i){ sieve[j] = true; }
+		for(int j = i + i; j < n; j += i){ sieve[j] = 0; }
 	}
 	return primes;
+}
+
+/**
+ *  @brief エラトステネスの篩による素数列挙
+ *
+ *  [0, n) の範囲に含まれる素数集合を可変長配列の形で返す。
+ *  計算量は \f$ \mathcal{O}(n \log{\log{n}}) \f$程度。
+ *
+ *  @param[in] n  素数集合の範囲
+ *  @return    素数集合の可変長配列による表現
+ */
+vector<int> eratosthenes(int n){
+	vector<bool> sieve(n);
+	return eratosthenes(sieve);
 }
 
 /**
